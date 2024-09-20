e411ddf8c35814205d3a84d906c5780ddb0e9b32
M-Court <mcourt@students.unwsp.edu>
2024-09-20 18:54:39 -0500

    Update program_5.py

--- program_5.py
+++ program_5.py
@@ -4,3 +4,67 @@
 # Also tax is 7%. 
 # Write a program the inputs the type of hot dog wanted and optional toppings.  
 # The program then displays the hot dog cost, tax and total cost. 
+
+
+#user chooses Hot Dog or Chili Dog
+order = input("Would you like a hot dog or chili dog? ")
+
+if "Hot Dog":
+		subtotal = 3.5
+if "Chili Dog":	
+		subtotal = 4.5
+
+#user decides if they want cheese
+cheese = input("Would you like cheese? ")
+if "Yes" add 0.5
+
+
+
+
+   new subtotal = subtotal + 0.5
+
+
+
+
+step_1 = input("Would you like a hot dog or chili dog? ")
+
+if input == "Hot Dog":
+	order = 3.5
+if input == "Chili Dog":	
+	order == 4.5
+
+cheese = input("Would you like cheese?")
+if input == "yes"
+	order2 = 3.5
+else: 
+	order
+
+peppers = input("Would you like peppers")
+
+{order + }
+	
+	
+#	if peppers == "yes" and cheese == "yes"
+	#	subtotal = "f{hot dog}" + 1.5
+#	if peppers == "yes" and cheese == "no"
+#		subtotal = "f{subtotal}" + 1
+	#if peppers == "no" and cheese == "yes"
+	#	subtotal == 
+  	
+
+
+#user decides if they want peppers
+
+if "no":
+    subtotal = subtotal
+
+#print subtotal 
+print(f'Your subtotal is ${subtotal:.2f}')
+
+#print tax
+tax = subtotal * 0.07
+print(f'Your tax is ${tax:.2f}')
+
+total = subtotal + tax
+#print total
+print(f'Your total is ${total:.2f}.')
